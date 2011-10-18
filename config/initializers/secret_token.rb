77505b348233f57886ba09452f6379d11062f3e8
Caelum Infra <infracaelumrj@gmail.com>
2011-10-18 19:42:10 -0200

    estrutura inicial do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CadastroDePessoas::Application.config.secret_token = '4b52363a4f13b31284be8af9985fdc4eee5d5f56c571664818e529e0828ca8ec17f403ab899e74cdcd7feb0b95d8156756053ca7605244971b1929920fd52daf'
